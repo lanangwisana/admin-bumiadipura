8c394b0d0553aa27cfb705f668e4ee9647e81f3e
Rofii29 <cibihijau12345@gmail.com>
2026-01-27 02:45:04 -0300

    fix dashboard admin

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,16 +3,22 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3hcMlk7sx9g0E43ecmmuUiTj1enF4eyw",
-  authDomain: "bumi-adipura-5abd8.firebaseapp.com",
-  projectId: "bumi-adipura-5abd8",
-  storageBucket: "bumi-adipura-5abd8.firebasestorage.app",
-  messagingSenderId: "1015566219694",
-  appId: "1:1015566219694:web:ed24031bfa5e0bafd1703f",
-  measurementId: "G-1RSTV4NSHB"
+  apiKey: "AIzaSyComvKBpm5xRMCLHeqSoq1bmkrwrdzQ7Qk",
+  authDomain: "bumi-adipura-8ed0a.firebaseapp.com",
+  projectId: "bumi-adipura-8ed0a",
+  storageBucket: "bumi-adipura-8ed0a.firebasestorage.app",
+  messagingSenderId: "895579623434",
+  appId: "1:895579623434:web:c0ef256fc55cf6e0bd0ff3",
+  measurementId: "G-1QWESVPHGR"
 };
 
+// Main app for general use
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Secondary app for creating new users without affecting current session
+const secondaryApp = initializeApp(firebaseConfig, 'SecondaryApp');
+export const secondaryAuth = getAuth(secondaryApp);
+
 export default app;
