1904eb0dcdc7229735faab101cc96948d80ae6b1
lanangwisana <lanangwisana@gmail.com>
2026-02-04 18:08:54 +0700

    update index: rendering feault event image

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export { auth, db, secondaryAuth } from './firebase';
-export { APP_ID, LOGO_URL, GEMINI_API_KEY, GEMINI_API_URL } from './constants';
+export { APP_ID, LOGO_URL, DEFAULT_EVENT_IMAGE, GEMINI_API_KEY, GEMINI_API_URL } from './constants';
